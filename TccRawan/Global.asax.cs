21bfb5e75cdd076859de96358f4bf98a0f2da2d6
RawanSaqqa <rawannezars@gmail.com>
2020-11-08 22:20:33 +0200

    first Change

--- TccRawan/Global.asax.cs
+++ TccRawan/Global.asax.cs
@@ -7,6 +7,7 @@ using System.Web.SessionState;
 
 namespace TccRawan
 {
+    //test
     public class Global : System.Web.HttpApplication
     {
         protected void Application_Start(object sender, EventArgs e)
