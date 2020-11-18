7e453deead41ec2eee7464d080c8b69a519313c6
RawanSaqqa <rawannezars@gmail.com>
2020-11-18 18:14:31 +0200

    edit2

---
+++ RawanApp/HF.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RawanApp
+{
+    public partial class HF : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if(HiddenField1.Value==string.Empty)
+            
+                HiddenField1.Value = "0";
+
+            HiddenField1.Value = (Convert.ToInt32(HiddenField1.Value) + 1).ToString();
+            Label1.Text = HiddenField1.Value;
+        }
+    }
+}
